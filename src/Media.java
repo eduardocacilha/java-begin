e301e0758aa0ff308dfe70afe3eb9a346ae67bdf
Eduardo Cacilha <ecacilhaa@gmail.com>
2026-01-30 15:52:02 -0300

    first commit with java

---
+++ src/Media.java
@@ -0,0 +1,9 @@
+public class Media {
+    public static void main(String[] args) {
+        double nota1 = 8.2;
+        double nota2 = 9.5;
+        double media = (nota1+nota2)/2;
+
+        System.out.printf("Media: %.2f%n", media);
+    }
+}
