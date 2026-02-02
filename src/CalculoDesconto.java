530b2aba7c8b91a277c0d3bf5f7ebf780ba48066
Eduardo Cacilha <ecacilhaa@gmail.com>
2026-02-02 09:52:03 -0300

    Primeiras tasks com java finalizadas - alura

--- src/CalculoDesconto.java
+++ src/CalculoDesconto.java
@@ -1,2 +1,9 @@
 public class CalculoDesconto {
+    public static void main(String[] args) {
+        double precoOriginal = 10.0;
+        double percentualDesconto = 10.0;
+        double valorComDesconto = precoOriginal*(percentualDesconto/100);
+
+        System.out.printf("O valor original: %.2f foi para %.2f com %.2f por cento desconto", precoOriginal, valorComDesconto, percentualDesconto);
+    }
 }
--- src/ConversaoDolaresReais.java
+++ src/ConversaoDolaresReais.java
@@ -1,2 +1,8 @@
 public class ConversaoDolaresReais {
+    public static void main(String[] args) {
+        double valorEmDolares = 15.7;
+        double valorEmReais = valorEmDolares*4.94;
+
+        System.out.printf("O valor de: %.2f em dolares foi para %.2f em reais",valorEmDolares, valorEmReais);
+    }
 }
